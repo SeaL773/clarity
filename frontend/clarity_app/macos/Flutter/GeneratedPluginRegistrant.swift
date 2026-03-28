aa692a2d1653c183c6d7d703d785de1b356fdb2c
Seal773 <wanglizhoujerry@gmail.com>
2026-03-28 19:46:41 -0400

    fix: upgrade record to v6 for Flutter compatibility

--- frontend/clarity_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/clarity_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,12 @@ import Foundation
 
 import file_picker
 import flutter_local_notifications
-import record_darwin
+import record_macos
 import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
-  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
+  RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
 }
